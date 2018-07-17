a82e91cb2a9e0511a3eba59a3fb4510a2dc88607
ningmoxin <ningmoxin@gmail.com>
2018-07-17 18:51:45 +0800

    ＬＩＮＥ通知家長

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,4 +1,15 @@
 class AttendancesController < ApplicationController
+	require 'oauth2'
+	require 'net/http'
+	require 'openssl'
+	require 'uri'
+	require 'json'
+	AUTH_SITE = 'https://notify-bot.line.me'
+	API_SITE  = 'https://notify-api.line.me'
+
+	CLIENT_ID     = $liensettings["client_id"]
+	CLIENT_SECRET = $liensettings["client_secret"]
+	CALLBACK      = File.join($liensettings["callback"], "oauth/callback")
 	def new
 		@attendance = Attendance.new
 		@attendance_top10 = Attendance.order("id desc").limit(10)
@@ -20,20 +31,20 @@ class AttendancesController < ApplicationController
 	end 	
 	def check(student)
 		@attendance_by_student = Attendance.getAttendance(student)
-		if (@attendance_by_student.size > 0)
-			@lastAttendance = @attendance_by_student.first
-			if((Time.now - @lastAttendance.start_time) / 60 > 10)
-				if (@lastAttendance.end_time.nil? and (Date.today - @lastAttendance.start_time.to_date).to_i < 1)
-					setFinishClass(student,@lastAttendance)
-				else
-					setStartClass(student)
-				end
-			else
-				flash[:error]  = "打卡失敗，時間過近，上下課時間至少要10分鐘以上。"
-			end
-		else
+		# if (@attendance_by_student.size > 0)
+		# 	@lastAttendance = @attendance_by_student.first
+		# 	if((Time.now - @lastAttendance.start_time) / 60 > 10)
+		# 		if (@lastAttendance.end_time.nil? and (Date.today - @lastAttendance.start_time.to_date).to_i < 1)
+		# 			setFinishClass(student,@lastAttendance)
+		# 		else
+		# 			setStartClass(student)
+		# 		end
+		# 	else
+		# 		flash[:error]  = "打卡失敗，時間過近，上下課時間至少要10分鐘以上。"
+		# 	end
+		# else
 			setStartClass(student)
-		end
+		# end
 	end
 	def setStartClass (student)
 		@attendance = Attendance.new
@@ -46,7 +57,9 @@ class AttendancesController < ApplicationController
 		contacts = student.contacts
 		if (!contacts.nil?)
 			contacts.each do |c|
-				if (c.)
+				if (c.notify_demand and (!c.line_token.nil? and !c.line_token.empty?))
+					send_message(c.line_token, c.name + "您好：您的愛子" + student.name + "已經上課囉！於" + @attendance.start_time.to_s)
+				end
 			end
 		end
 	end
@@ -54,5 +67,37 @@ class AttendancesController < ApplicationController
 	def setFinishClass (student,attendance)
 		attendance.end_time = Time.now
 		attendance.save
+	end
+
+	def send_message (token, message)
+		p message
+		res = msg(token, message)
+
+		case res
+		when Net::HTTPSuccess
+			@result = "successful"
+		else
+			j = JSON.parse(res.body)
+			@result = "[status:#{j["status"]}] #{j["message"]}"
+		end
+	end
+
+	def msg(token, message)
+		url = URI.parse(File.join(API_SITE, 'api/notify'))
+		req = Net::HTTP::Post.new(url.path)
+		req.content_type = "application/x-www-form-urlencoded"
+		req['Authorization'] = "Bearer #{token}"
+		req.set_form_data({
+			"message" => message
+		})
+
+		http = Net::HTTP.new(url.host, url.port)
+		http.use_ssl = true
+		http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+
+		http.start { |h|
+			h.request(req)
+		}
 	end
 end
